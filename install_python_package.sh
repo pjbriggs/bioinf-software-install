265f53dfb1e67452f207c9ac051e3e3c46565bb6
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-01-22 10:56:05 +0000

    Remove install_package() from install_python_package.sh (moved to functions.sh).

--- install_python_package.sh
+++ install_python_package.sh
@@ -7,42 +7,6 @@
 #
 . $(dirname $0)/functions.sh
 #
-function install_package() {
-    echo -n Entering directory $(package_dir $1)...
-    cd $(package_dir $1)
-    echo done
-    echo -n Checking python...
-    if [ -f $3 ] && [ -x $3 ] ; then
-	echo ok
-    else
-	echo FAILED
-	echo $3 is not an executable file
-	if [ -d $3 ] ; then
-	    echo Did you mean $3/python?
-	fi
-	echo ERROR $3 is not an executable file >&2
-	exit 1
-    fi
-    echo -n Detecting python version...
-    local python_version=python$(python_version $3)
-    echo $python_version
-    local lib_dir=$2/lib/$python_version/site-packages
-    echo -n Making $lib_dir...
-    mkdir -p $lib_dir
-    echo done
-    echo -n Prepending $lib_dir to PYTHONPATH...
-    prepend_path PYTHONPATH $lib_dir
-    echo done
-    echo -n Installing into $2...
-    local install_cmd="$3 setup.py install --prefix=$2"
-    $install_cmd > install.log 2>&1
-    if [ "$?" -eq 0 ] ; then
-	echo done
-    else
-	echo FAILED
-	exit 1
-    fi
-}
 # Main script
 PYTHON=$1
 TARGZ=$2
@@ -59,6 +23,6 @@ echo Version $(package_version $TARGZ)
 echo Using Python from $(dirname $PYTHON)
 echo Installing under $INSTALL_DIR
 unpack_archive $TARGZ
-install_package $TARGZ $INSTALL_DIR $PYTHON
+install_python_package $TARGZ $INSTALL_DIR $PYTHON
 ##
 #
