6bb7974b9bff5d3b7cdd054118d1f14f6cfbe370
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-02-13 17:12:38 +0000

    Check if R or BioConductor packages already installed, use --force to reinstall.

--- install_bioc_package.sh
+++ install_bioc_package.sh
@@ -8,11 +8,15 @@
 . $(dirname $0)/functions.sh
 #
 # Main script
+if [ "$1" == "--force" ] ; then
+    force_install=yes
+    shift
+fi
 R_EXE=$1
 PACKAGE=$2
 INSTALL_DIR=$3
 if [ -z "$R_EXE" ] || [ -z "$PACKAGE" ] || [ -z "$INSTALL_DIR" ] ; then
-  echo Usage: $(basename $0) R_EXE PACKAGE INSTALL_DIR
+  echo Usage: $(basename $0) \[--force\] R_EXE PACKAGE INSTALL_DIR
   exit 1
 fi
 R_EXE=$(full_path $R_EXE)
@@ -31,6 +35,17 @@ fi
 echo -n Prepending $INSTALL_DIR to R_LIBS...
 prepend_path R_LIBS $INSTALL_DIR
 echo done
+echo -n Checking if $PACKAGE_NAME is already installed...
+if [ ! -z "$(R_package_installed $R_EXE $PACKAGE_NAME)" ] ; then
+    echo yes
+    if [ -z "$force_install" ] ; then
+	echo $PACKAGE_NAME already installed, use --force to override
+	exit
+    fi
+    echo Reinstalling $PACKAGE_NAME
+else
+    echo no
+fi
 echo -n Running biocLite.R in R...
 $R_EXE --vanilla &> $PACKAGE_NAME.$R_VER.install.log <<EOF
 source("http://bioconductor.org/biocLite.R")
