b3743d192113bbb039afd9288dbc80e5294f5719
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-02-14 10:14:29 +0000

    install_python_package.sh updated to check if package is already installed, only proceed with --force.

--- install_python_package.sh
+++ install_python_package.sh
@@ -9,21 +9,37 @@
 . $(dirname $0)/functions.sh
 #
 # Main script
+if [ "$1" == "--force" ] ; then
+    force_install=yes
+    shift
+fi
 PYTHON=$1
 TARGZ=$2
 INSTALL_DIR=$3
 if [ -z "$PYTHON" ] || [ -z "$TARGZ" ] || [ -z "$INSTALL_DIR" ] ; then
-  echo Usage: $(basename $0) PYTHON TARGZ INSTALL_DIR
+  echo Usage: $(basename $0) \[--force\] PYTHON TARGZ INSTALL_DIR
   exit 1
 fi
 PYTHON=$(full_path $PYTHON)
 PYTHON_VER=$(python_version $PYTHON)
+PACKAGE_NAME=$(package_name $TARGZ)
 INSTALL_DIR=$(full_path $INSTALL_DIR)/$PYTHON_VER
-echo "## Install $(package_name $TARGZ) ##"
+echo "## Install $PACKAGE_NAME ##"
 echo Archive $TARGZ
 echo Version $(package_version $TARGZ)
 echo Using Python from $(dirname $PYTHON)
 echo Installing under $INSTALL_DIR
+echo -n Checking if $PACKAGE_NAME is already installed...
+if [ ! -z "$(python_package_installed $PYTHON $PACKAGE_NAME)" ] ; then
+    echo yes
+    if [ -z "$force_install" ] ; then
+	echo $PACKAGE_NAME already installed, use --force to override
+	exit
+    fi
+    echo Reinstalling $PACKAGE_NAME
+else
+    echo no
+fi 
 unpack_archive $TARGZ
 install_python_package $PYTHON $TARGZ $INSTALL_DIR
 clean_up $TARGZ
