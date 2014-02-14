eb79d1c9b3b79f84928661e7ab205f9effac9531
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-02-14 11:17:58 +0000

    functions.sh: updated prepend_path to deal with path element already being present (using new function remove_path).

--- functions.sh
+++ functions.sh
@@ -58,12 +58,36 @@ function prepend_path() {
     # Prepend path to path-type variable
     # 1: path variable name e.g. PATH
     # 2: path to prepend e.g. /home/$USER/bin
+    remove_path $1 $2
     eval local path=\$$1
-    if [ -z "$path" ] ; then
-	eval $1=$2
+    if [ ! -z "$path" ] ; then
+	new_path=$2:$path
     else
-	eval $1=$2:$path
+	new_path=$2
     fi
+    eval $1=$new_path
+    export $1
+}
+function remove_path() {
+    # Remove path from path-type variable
+    # 1: path variable name e.g. PATH
+    # 2: path to remove
+    eval local path=\$$1
+    local new_path=
+    if [ ! -z "$path" ] ; then
+	while [ ! -z "$path" ] ; do
+	    local el=$(echo $path | cut -d":" -f1)
+	    path=$(echo $path | cut -s -d":" -f2-)
+	    if [ ! -z "$el" ] && [ "$el" != "$2" ] ; then
+		if [ -z "$new_path" ] ; then
+		    new_path=$el
+		else
+		    new_path=$new_path:$el
+		fi
+	    fi
+	done
+    fi
+    eval $1=$new_path
     export $1
 }
 function wget_url() {
