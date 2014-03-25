c61b15cc9698d2d77de56cc78c32acec24dddaef
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-03-25 11:30:33 +0000

    install_blast.sh: new script to fetch and install NCBI Blast+ binaries.

---
+++ install_blast.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Fetch and install NCBI blast+ binaries
+#
+. $(dirname $0)/functions.sh
+#
+BLAST_VERSION=$1
+INSTALL_DIR=$2
+if [ -z "$BLAST_VERSION" ] || [ -z "$INSTALL_DIR" ] ; then
+  echo Usage: $(basename $0) BLAST_VERSION INSTALL_DIR
+  echo Downloads and installs NCBI Blast+ binaries to INSTALL_DIR/ncbi-blast/VERSION
+  exit 1
+fi
+BLAST_VERSION=$(echo $BLAST_VERSION | tr -d '+')
+echo Install NCBI Blast version ${BLAST_VERSION}+
+BASE_URL=ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${BLAST_VERSION}
+TARGZ_FILE=ncbi-blast-${BLAST_VERSION}+-x64-linux.tar.gz
+MD5_FILE=ncbi-blast-${BLAST_VERSION}+-x64-linux.tar.gz.md5
+TARGZ_URL=${BASE_URL}/$TARGZ_FILE
+MD5_URL=${BASE_URL}/$MD5_FILE
+if [ -f $TARGZ_FILE ] ; then
+  echo ERROR $TARGZ_FILE already exists >&2
+  exit 1
+fi
+wget_url $TARGZ_URL
+wget_url $MD5_URL
+echo -n Checking MD5 checksum...
+md5sum -c $MD5_FILE
+if [ $? -ne 0 ] ; then
+  echo ERROR MD5 check failed >&2
+  exit 1
+fi
+unpack_archive --no-package-dir-check $TARGZ_FILE
+BLAST_DIR=ncbi-blast-${BLAST_VERSION}+
+if [ ! -d $BLAST_DIR ] ; then
+  echo ERROR no directory $BLAST_DIR >&2
+fi
+INSTALL_DIR=$(full_path $INSTALL_DIR)/ncbi-blast/${BLAST_VERSION}+
+echo -n Creating $INSTALL_DIR...
+mkdir -p $INSTALL_DIR
+echo done
+copy_contents $BLAST_DIR $INSTALL_DIR
+clean_up_dir $BLAST_DIR
+echo "#%Module1.0"
+echo "## ncbi-blast ${BLAST_VERSION}+ modulefile"
+echo "prepend-path PATH            $INSTALL_DIR/bin"
+echo 
+##
+#
+
+
