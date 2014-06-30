6a53ccd406e3fbaacd645190735d037e011d9780
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-06-30 14:42:09 +0100

    install_abyss.sh: substantial update to utilise wrapper functions and allow --enable-max-kmer to be set explicitly.

--- install_abyss.sh
+++ install_abyss.sh
@@ -10,72 +10,68 @@
 #
 . $(dirname $0)/import_functions.sh
 #
+# Defaults
+BOOST_INCL=/usr/include/boost
+SPARSEHASH_INCL=/usr/include/google/sparsehash
+MAX_KMER=128
+#
+# Command line
+while [ $# -gt 2 ] ; do
+    case "$1" in
+	--boost-incl)
+	    shift
+	    BOOST_INCL=$1
+	    ;;
+	--sparsehash-incl)
+	    shift
+	    SPARSEHASH_INCL=$1
+	    ;;
+	--max-kmer)
+	    shift
+	    MAX_KMER=$1
+	    ;;
+	--*)
+	    echo ERROR unrecognised option $1 >&2
+	    exit 1
+	    ;;
+    esac
+    shift
+done
 TARGZ=$1
 INSTALL_DIR=$2
 if [ -z "$TARGZ" ] || [ -z "$INSTALL_DIR" ] ; then
     echo Usage: $(basename $0) \[OPTIONS\] TARGZ INSTALL_DIR
     echo Installs ABYSS to INSTALL_DIR/abyss/VERSION
+    echo --boost-incl BOOST_INCL: set directory for boost headers
+    echo --sparsehash-incl SPARSEHASH_INCL: set directory for sparsehash headers
+    echo --max-kmer LENGTH: set maximum kmer length \(default 128\)
     exit 1
 fi
+# Start
 ABYSS_DIR=$(package_dir $TARGZ)
 ABYSS_VER=$(package_version $TARGZ)
 INSTALL_DIR=$(full_path $INSTALL_DIR)/abyss/$ABYSS_VER
 echo Build ABYSS from $TARGZ
 echo Version $ABYSS_VER
-BOOST_INCL=/usr/include/boost
-echo -n Checking for $BOOST_INCL...
-if [ ! -d "$BOOST_INCL" ] ; then
-    echo FAILED
-    echo ERROR $BOOST_INCL not found >&2
-    exit 1
-else
-    echo ok
-fi
-SPARSEHASH_INCL=/usr/include/google/sparsehash
-echo -n Checking for $SPARSEHASH_INCL...
-if [ ! -d "$SPARSEHASH_INCL" ] ; then
-    echo FAILED
-    echo ERROR $SPARSEHASH_INCL not found >&2
-    exit 1
-else
-    echo ok
-fi
+# Set up build log
+LOG_FILE=$(pwd)/install.abyss.$ABYSS_VER.log
+clean_up_file $LOG_FILE
+# Check include directories exist
+check_directory $BOOST_INCL
+check_directory $SPARSEHASH_INCL
+# Unpack source code
 unpack_archive $TARGZ
-if [ ! -d $ABYSS_DIR ] ; then
-  echo ERROR no directory $ABYSS_DIR found >&2
-  exit 1
-fi
 echo Moving to $ABYSS_DIR
+# Do build and install
 cd $ABYSS_DIR
-echo -n Running configure...
-./configure --prefix=$INSTALL_DIR --enable-maxk=128 >> build.log 2>&1
-if [ $? -ne 0 ] ; then
-  echo FAILED
-  echo ERROR make returned non-zero exit code >&2
-  exit 1
-else
-  echo ok
-fi
-echo -n Running make...
-make >> build.log 2>&1
-if [ $? -ne 0 ] ; then
-  echo FAILED
-  echo ERROR make returned non-zero exit code >&2
-  exit 1
-else
-  echo ok
-fi
-echo -n Creating $INSTALL_DIR...
-mkdir -p $INSTALL_DIR
-echo done
-echo -n Running make install...
-make install >> build.log 2>&1
-if [ $? -ne 0 ] ; then
-  echo FAILED
-  echo ERROR make install returned non-zero exit code >&2
-  exit 1
-else
-  echo ok
-fi
+export CPPFLAGS="-I$BOOST_INCL -I$SPARSEHASH_INCL"
+do_configure --log $LOG_FILE \
+    --prefix=$INSTALL_DIR --enable-maxk=128
+do_make --log $LOG_FILE
+create_directory $INSTALL_DIR
+do_make --log $LOG_FILE install
+# Finish up
 cd ..
 clean_up $TARGZ
+##
+#
