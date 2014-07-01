ebc3a2d5ed3c3dba659be5aa7113c8a2e2828cef
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-07-01 15:15:49 +0100

    build_python.py: various updates including using wrapper functions and trapping for ez_setup.py failed.

--- build_python.sh
+++ build_python.sh
@@ -11,28 +11,23 @@ if [ -z "$TARGZ" ] || [ -z "$INSTALL_DIR" ] ; then
   echo Installs python to INSTALL_DIR/python/VERSION
   exit 1
 fi
-#
 PYTHON_DIR=$(package_dir $TARGZ)
 PYTHON_VER=$(package_version $TARGZ)
 INSTALL_DIR=$(full_path $INSTALL_DIR)/python/$PYTHON_VER
 echo Build python from $TARGZ
 echo Version $PYTHON_VER
-echo "## BUILD AND INSTALL PYTHON ##"
+# Set up log file
+LOG_FILE=$(pwd)/install.python.$PYTHON_VER.log
+clean_up_file $LOG_FILE
+# Build and install Python
 unpack_archive $TARGZ
-if [ ! -d $PYTHON_DIR ] ; then
-  echo ERROR no directory $PYTHON_DIR found >&2
-  exit 1
-fi
-echo -n Building in $PYTHON_DIR...
+echo Moving to $PYTHON_DIR
 cd $PYTHON_DIR
-./configure --prefix=$INSTALL_DIR > build.log 2>&1
-make >> build.log 2>&1
-echo done
-echo -n Installing to $INSTALL_DIR...
-mkdir -p $INSTALL_DIR
-make install > install.log 2>&1
-echo done
-echo "## INSTALL EASY_INSTALL/PIP ##"
+do_configure --log $LOG_FILE --prefix=$INSTALL_DIR
+do_make --log $LOG_FILE
+create_directory $INSTALL_DIR
+do_make --log $LOG_FILE install
+# Install ez_setup and pip
 EZ_SETUP_URL=http://peak.telecommunity.com/dist/ez_setup.py
 wget_url $EZ_SETUP_URL
 if [ ! -f ez_setup.py ] ; then
@@ -41,11 +36,17 @@ if [ ! -f ez_setup.py ] ; then
 fi
 PYTHON_BIN=$INSTALL_DIR/bin
 echo -n Installing easy_install...
-$PYTHON_BIN/python ez_setup.py >> install.log 2>&1
+$PYTHON_BIN/python ez_setup.py >>$LOG_FILE 2>&1
+if [ $? -ne 0 ] ; then
+    echo FAILED
+    echo ez_setup.py exited with non-zero status >&2
+    exit 1
+fi
 echo done
 echo -n Installing pip...
-$PYTHON_BIN/easy_install pip >> install.log 2>&1
+$PYTHON_BIN/easy_install pip >>$LOG_FILE 2>&1
 echo done
+# Finish up
 cd ..
 clean_up $TARGZ
 ##
