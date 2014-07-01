e1a6ca2552ec87abe107d2392e4ab6ae113cf3ea
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-07-01 12:39:56 +0100

    install_fastx_toolkit.sh: updated to use wrapper functions and make script more concise.

--- install_fastx_toolkit.sh
+++ install_fastx_toolkit.sh
@@ -2,6 +2,10 @@
 #
 # Install fastx_toolkit and gtextutils
 #
+# Works for:
+# fastx_toolkit 0.0.14 & libgtextutils 0.7
+# fastx_toolkit 0.0.13.2 & libgtextutils 0.6.1
+#
 . $(dirname $0)/import_functions.sh
 #
 FASTX_TOOLKIT_TARGZ=$1
@@ -18,55 +22,39 @@ LIBGTEXTUTILS_DIR=$(package_dir $LIBGTEXTUTILS_TARGZ)
 LIBGTEXTUTILS_VER=$(package_version $LIBGTEXTUTILS_TARGZ)
 INSTALL_DIR=$(full_path $INSTALL_DIR)/fastx_toolkit/$FASTX_TOOLKIT_VER
 GTEXTUTILS_INSTALL_DIR=$INSTALL_DIR/gtextutils-$LIBGTEXTUTILS_VER
+# Set up build log
+LOG_FILE=$(pwd)/install.fastx_toolkit.$FASTX_TOOLKIT_VER.log
+clean_up_file $LOG_FILE
+# Build libgtextutils
 echo Build libgtextutils from $LIBGTEXTUTILS_TARGZ
 echo Version $LIBGTEXTUTILS_VER
 unpack_archive $LIBGTEXTUTILS_TARGZ
-if [ ! -d $LIBGTEXTUTILS_DIR ] ; then
-  echo ERROR no directory $LIBGTEXTUTILS_DIR found >&2
-  exit 1
-fi
-echo -n Building in $LIBGTEXTUTILS_DIR...
+echo Moving to $LIBGTEXTUTILS_DIR
 cd $LIBGTEXTUTILS_DIR
-./configure --prefix=$GTEXTUTILS_INSTALL_DIR >> build.log 2>&1
-make >> build.log 2>&1
-if [ $? -ne 0 ] ; then
-  echo FAILED
-  echo make failed for libgtextutils >&2
-  exit 1
-else
-  echo done
-fi
-echo -n Installing libgtextutils in $GTEXTUTILS_INSTALL_DIR...
-make install >> build.log 2>&1
-echo done
+do_configure --log $LOG_FILE \
+    --prefix=$GTEXTUTILS_INSTALL_DIR
+do_make --log $LOG_FILE
+do_make --log $LOG_FILE install
 cd ..
 clean_up $LIBGTEXTUTILS_TARGZ
+# Build fastx_toolkit
 echo Build fastx_toolkit from $FASTX_TOOLKIT_TARGZ
 echo Version $FASTX_TOOLKIT_VER
 unpack_archive $FASTX_TOOLKIT_TARGZ
-if [ ! -d $FASTX_TOOLKIT_DIR ] ; then
-  echo ERROR no directory $FASTX_TOOLKIT_DIR found >&2
-  exit 1
-fi
 echo -n Adding $GTEXTUTILS_INSTALL_DIR/lib to LD_LIBRARY_PATH...
 prepend_path LD_LIBRARY_PATH $GTEXTUTILS_INSTALL_DIR/lib
 echo done
-echo -n Building in $FASTX_TOOLKIT_DIR...
+echo Moving to $FASTX_TOOLKIT_DIR
 cd $FASTX_TOOLKIT_DIR
-./configure --prefix=$INSTALL_DIR GTEXTUTILS_CFLAGS="-I$GTEXTUTILS_INSTALL_DIR/include/gtextutils" GTEXTUTILS_LIBS=-lgtextutils >> build.log 2>&1
-make >> build.log 2>&1
-if [ $? -ne 0 ] ; then
-  echo FAILED
-  echo make failed for fastx_toolkit >&2
-  exit 1
-else
-  echo done
-fi
-echo -n Installing fastx_toolkit in $INSTALL_DIR...
-make install >> build.log 2>&1
-echo done
+do_configure --log $LOG_FILE \
+    --prefix=$INSTALL_DIR \
+    GTEXTUTILS_CFLAGS="-I$GTEXTUTILS_INSTALL_DIR/include/gtextutils" \
+    GTEXTUTILS_LIBS=-lgtextutils
+do_make --log $LOG_FILE
+do_make --log $LOG_FILE install
 cd ..
 clean_up $FASTX_TOOLKIT_TARGZ
+# Suggest module file
 echo "#%Module1.0"
 echo "## fastx_toolkit $FASTX_TOOLKIT_VER modulefile"
 echo "prepend-path PATH            $INSTALL_DIR/bin"
