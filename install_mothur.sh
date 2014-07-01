241e532c17d467cd95d1decb1e4080ca507c60ad
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-07-01 14:25:38 +0100

    install_mothur.sh: use wrapper functions to make script more concise.

--- install_mothur.sh
+++ install_mothur.sh
@@ -2,6 +2,9 @@
 #
 # Install mothur
 #
+# Works for 1.32.1
+#           1.33.2
+#
 . $(dirname $0)/import_functions.sh
 #
 ZIP=$1
@@ -16,19 +19,20 @@ MOTHUR_VER=$(echo ${ZIP%.zip} | cut -d. -f2-)
 INSTALL_DIR=$(full_path $INSTALL_DIR)/mothur/$MOTHUR_VER
 echo Build mothur from $ZIP
 echo Version $MOTHUR_VER
+# Set up log file
+LOG_FILE=$(pwd)/install.mothur.$MOTHUR_VER.log
+clean_up_file $LOG_FILE
+# Unpack
 unpack_archive --no-package-dir-check $ZIP
-if [ ! -d $MOTHUR_DIR ] ; then
-  echo ERROR no directory $MOTHUR_DIR found >&2
-  exit 1
-fi
+check_directory $MOTHUR_DIR
 echo -n Moving $MOTHUR_DIR to $MOTHUR_DIR.$MOTHUR_VER...
 mv -f $MOTHUR_DIR $MOTHUR_DIR.$MOTHUR_VER
 MOTHUR_DIR=$MOTHUR_DIR.$MOTHUR_VER
 echo done
 clean_up_dir __MACOSX
-echo -n Moving to $MOTHUR_DIR...
+# Build Mothur
+echo Moving to $MOTHUR_DIR
 cd $MOTHUR_DIR
-echo done
 echo -n Updating makefile...
 sed -i 's/^USECOMPRESSION ?= no/USECOMPRESSION ?= yes/' makefile
 sed -i 's/TARGET_ARCH += -arch x86_64/#TARGET_ARCH += -arch x86_64/' makefile
@@ -37,23 +41,11 @@ echo done
 echo -n Updating uchime_src/mk...
 sed -i 's/LINK_OPTS=-static/LINK_OPTS=/' uchime_src/mk
 echo done
-echo -n Running make...
-make > ../install.mothur.$MOTHUR_VER.log 2>&1
-if [ $? -ne 0 ] ; then
-  echo FAILED
-  echo See log file install.mothur.$MOTHUR_VER.log for more information
-  echo make failed for mothur >&2
-  exit 1
-else
-  echo done
-  clean_up_file ../install.mothur.$MOTHUR_VER.log
-fi
+do_make --log $LOG_FILE
+# Install files
+create_directory $INSTALL_DIR
+copy_files mothur uchime $INSTALL_DIR
 cd ..
-echo -n Creating $INSTALL_DIR...
-mkdir -p $INSTALL_DIR
-echo done
-copy_file $MOTHUR_DIR/mothur $INSTALL_DIR
-copy_file $MOTHUR_DIR/uchime $INSTALL_DIR
 clean_up_dir $MOTHUR_DIR
 echo "#%Module1.0"
 echo "## mothur $MOTHUR_VER modulefile"
